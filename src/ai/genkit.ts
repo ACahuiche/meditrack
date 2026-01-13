194eda46a28666b0e0c60824d4faa8232429ba82
Alberto Cahuiche <albertomanuel.cahuiche@gmail.com>
2026-01-13 19:00:51 +0000

    me arrojo 2 UNKNOWN: Getting metadata from plugin failed with error: Cou

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
-import {genkit} from 'genkit';
+import nextGenkit from '@genkit-ai/next';
 import {googleAI} from '@genkit-ai/google-genai';
+import {genkit} from 'genkit';
 
-export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
-});
+export const ai = nextGenkit(
+  genkit({
+    plugins: [googleAI()],
+    model: 'googleai/gemini-2.5-flash',
+  })
+);
