378d6984e39b899cdfc3c2251c59c22c5a457c95
Alberto Cahuiche <albertomanuel.cahuiche@gmail.com>
2026-01-13 18:50:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4200779032-b68bb",
+  "appId": "1:1065774035380:web:aa302c937c74a28bfc02e1",
+  "apiKey": "AIzaSyBxFmxJWi76RXKll34Ps4bZdvV-Sq2ux-I",
+  "authDomain": "studio-4200779032-b68bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1065774035380"
+};
