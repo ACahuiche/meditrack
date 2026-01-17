f06219d53269b535cb18bd8f431c4f40a9946faa
Alberto Cahuiche <albertomanuel.cahuiche@gmail.com>
2026-01-17 21:39:34 +0000

    veo que la app web tiene un favicon, pero tambien es una PWA asi que la

---
+++ src/app/icon.tsx
@@ -0,0 +1,35 @@
+import { ImageResponse } from 'next/og';
+import { AppLogo } from '@/components/icons';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          borderRadius: '6px',
+          background: '#f0f9ff', // Background color (hsl(210 100% 97%))
+          color: '#3b82f6', // Primary color (hsl(208 92% 60%))
+        }}
+      >
+        <AppLogo style={{ width: '80%', height: '80%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
